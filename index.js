86bea477e36d763694298b8d9d2f067598eafb95
navyakallam <109719155+navyakallam@users.noreply.github.com>
2025-05-13 19:57:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, Elevate Labs, This is my before pushing'));
+app.get('/', (req, res) => res.send('Hello, Elevate Labs, This is my after pushing'));
 app.listen(3000, () => console.log('App running on port 3000'));
